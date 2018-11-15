c7266944e16861a0b230ff85c38a46d0e0a58ef3
bbbrtk <sobkowiak.bart@gmail.com>
2018-11-15 15:06:07 +0100

    add clipped images

--- project/filed_recognition.py
+++ project/filed_recognition.py
@@ -1,2 +1,42 @@
+import numpy as np
+import cv2
+import os
+import matplotlib.pyplot as plt
+
+from skimage import data, io, filters, exposure, measure, segmentation
+from skimage.filters import rank
+from skimage import img_as_float, img_as_ubyte
+from skimage.morphology import disk
+import skimage.morphology as mp
+from skimage import util
+from skimage.color import rgb2hsv, hsv2rgb, rgb2gray, gray2rgb
+from skimage.filters.edges import convolve
 
 print("start___")
+
+img_name = 'low_contrast.jpg'
+img = io.imread(os.path.join('resources/', img_name))
+img = rgb2gray(img)
+
+contours = measure.find_contours(img, 0.1)
+fig, ax = plt.subplots()
+ax.imshow(img, interpolation='nearest', cmap=plt.cm.gray)
+
+for n, contour in enumerate(contours):
+    # ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
+
+    # filtering
+    if (
+        contours[n-1][0, 1] - contour[0 , 1] < 5
+        or
+        contours[n - 1][0, 0] - contour[0, 0] < 5
+    ):
+        ax.plot(contour[:, 1], contour[:, 0], 'r', linewidth=2)
+
+
+ax.set_xticks([])
+ax.set_yticks([])
+plt.show()
+
+#io.imshow(img)
+#plt.show()
---
+++ project/opencv_samples.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+import os
+import matplotlib.pyplot as plt
+
+from skimage import data, io, filters, exposure, measure, segmentation
+from skimage.filters import rank
+from skimage import img_as_float, img_as_ubyte
+from skimage.morphology import disk
+import skimage.morphology as mp
+from skimage import util
+from skimage.color import rgb2hsv, hsv2rgb, rgb2gray, gray2rgb
+from skimage.filters.edges import convolve
+
+
+img_name = 'middle_contrast.jpg'
+
+img = cv2.imread('resources/' + img_name)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret, thresh = cv2.threshold(gray, 127, 255, 1)
+image, contours, h = cv2.findContours(thresh, 1, 2)
+
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+    print(len(approx))
+    if len(approx) == 5:
+        print("pentagon")
+        cv2.drawContours(img, [cnt], 0, 255, -1)
+    elif len(approx) == 3:
+        print("triangle")
+        cv2.drawContours(img, [cnt], 0, (0, 255, 0), -1)
+    elif len(approx) == 4:
+        print("square")
+        cv2.drawContours(img, [cnt], 0, (0, 0, 255), -1)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
