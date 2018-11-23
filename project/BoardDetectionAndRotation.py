68fe3376da9ecae5b1381f508db33279341cabda
Wiktor <gedin8@gmail.com>
2018-11-23 19:17:47 +0100

    W skrypcie "BoardDetection[...]" znajduje się funkcja "czarna pizza" ktora w miarę dokładnie transformuje naszą planszę do bialo-czarnej mapy (zależy od parametrów low/upp bound wykrywania niebieskiego koloru) //do dokalibrowania znalazłem też funkcję która taki czarno-biały obiekcik obrysowuję i zwraca (chyba) kontur dokladny lub przyblizony (kwardrat!) wiec dalej bawie sie w obsluge tego

--- project/BoardDetectionAndRotation.py
+++ project/BoardDetectionAndRotation.py
@@ -11,23 +11,23 @@ from skimage import io
 from skimage.color import rgb2gray
 
 print("start")
-
 img_name = '20181120_123100.jpg'
 img = cv2.imread(os.path.join('resources/Photos', img_name))
 #img = rgb2gray(img)
 
-
 def czarnaPizza(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) ##DO ZAKOMENTOWANIA JESLI
+    swapped = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  ##DO ZAKOMENTOWANIA JESLI
                                                ##OBRAZEK JEST ŁADOWANY PRZEZ io.imread
                                                ##(POTRZEBNE DLA cv2.imread)
+
+    #swapped =img                               #JEŚLI ŁADOWANE moudłem skimage
     bounds = [[0, 3, 94], [110, 170, 255]]
 
     lower = np.array(bounds[0], dtype="uint8")
     upper = np.array(bounds[1], dtype="uint8")
 
-    mask = cv2.inRange(img, lower, upper)
-    filtered = cv2.bitwise_and(img, img, mask=mask)
+    mask = cv2.inRange(swapped, lower, upper)
+    filtered = cv2.bitwise_and(swapped, swapped, mask=mask)
 
     thresh, dst = cv2.threshold(filtered, 0, 255, cv2.THRESH_BINARY)  # JESLI CHCESZ ZMIENIC KOLORY
                                                                   # DOPISZ _INV DO OSTATNIEGO ARGUMENTU
@@ -72,15 +72,14 @@ def czarnaPizza(img):
     # hull = cv2.convexHull(cnt)
     # print(cnt.shape)
     # small = cv2.resize(dst, (700, 700))
-    #cv2.drawContours(img, longestContour, -1, color=(255,0,0), thickness=5)
+    # cv2.drawContours(swapped, longestContour, -1, color=(255,0,0), thickness=5)
     #cv2.imwrite("image", dst)
     #plt.imshow(dst, cmap='gray')
-    # plt.imshow(img, cmap='gray')
+    # plt.imshow(swapped, cmap='gray')
     # plt.show()
-
     return output
 
-
-wynik = czarnaPizza(img)
-plt.imshow(wynik)
-plt.show()
+# wynik = czarnaPizza(img)
+# wynik = cv2.cvtColor(wynik, cv2.COLOR_BGR2RGB)
+# plt.imshow(wynik)
+# plt.show()
