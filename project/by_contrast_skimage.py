15cc2a100c3ee13688b736d50a5ba202be179ff8
bbbrtk <sobkowiak.bart@gmail.com>
2018-11-28 21:27:40 +0000

    add new images

--- project/by_contrast_skimage.py
+++ project/by_contrast_skimage.py
@@ -1,11 +1,16 @@
-import os
 import math
-import numpy as np
+import os
+
+import cv2
 import matplotlib.pyplot as plt
-from skimage import data, io, filters, exposure, measure, segmentation, draw, transform
-from skimage.morphology import disk, opening, dilation, square
-from skimage.color import rgb2hsv, hsv2rgb, rgb2gray, gray2rgb
+import numpy as np
 from board import BoardAssignment
+from skimage import io, exposure, measure, draw, filters
+from skimage.color import rgb2hsv, rgb2gray
+from skimage.morphology import disk, opening, dilation, square, closing, thin
+
+
+# =========== OTHERS ============
 
 
 def polygon_area(corners):
@@ -26,6 +31,7 @@ def polygon_area(corners):
 
 def find_n_greatest_areas(contours, n_times=3):
     """
+    calculate area of contour and check if should be in N-greatest contours array
     :param contours: array of arrays of contours coordinates
     :param n_times: number of contours to find
     :return: N greatest contours (greatest areas)
@@ -48,30 +54,24 @@ def find_n_greatest_areas(contours, n_times=3):
     return greatest_contours
 
 
-def check_if_circle(contours):
+def check_if_circle(contours, std=16, lenmin=200, lenmax=1200):
     """
     check by calculating std of distance between centroid and each contour point
     :param contours: array of arrays of contours coordinates
     :return: the most rounded contours
     """
-
     circles = []
     for n, contour in enumerate(contours):
         centroid = contour.mean(axis=0)
         distances = []
         for i in contour: distances.append(math.hypot(centroid[0] - i[0], centroid[1] - i[1]))
-        if np.std(distances) < 16 and 200 < len(contour) < 1200:
+        if np.std(distances) < std and lenmin < len(contour) < lenmax:
             circles.append(contour)
 
     return circles
 
 
-def create_mask(img_rgb, type='v', equation='>', saturation=0.75, remove=0, rejoin=5, show_mask=False):
-    # params:
-    saturation_param = saturation
-    remove_disk_param = remove
-    rejoin_square_param = rejoin
-
+def create_mask(img_rgb, hsv_type='v', equation='>', saturation=0.75, remove=0, rejoin=5, show_mask=False):
     # split to h s v
     img = rgb2hsv(img_rgb)
     h = img[:, :, 0]
@@ -80,27 +80,27 @@ def create_mask(img_rgb, type='v', equation='>', saturation=0.75, remove=0, rejo
 
     # create mask
     if equation == '>':
-        if type == 'h':
-            mask = (h > saturation_param).astype(np.uint8)
-        elif type == 's':
-            mask = (s > saturation_param).astype(np.uint8)
+        if hsv_type == 'h':
+            mask = (h > saturation).astype(np.uint8)
+        elif hsv_type == 's':
+            mask = (s > saturation).astype(np.uint8)
         else:
-            mask = (v > saturation_param).astype(np.uint8)
+            mask = (v > saturation).astype(np.uint8)
 
-    if equation == '<':
-        if type == 'h':
-            mask = (h < saturation_param).astype(np.uint8)
-        elif type == 's':
-            mask = (s < saturation_param).astype(np.uint8)
+    else:
+        if hsv_type == 'h':
+            mask = (h < saturation).astype(np.uint8)
+        elif hsv_type == 's':
+            mask = (s < saturation).astype(np.uint8)
         else:
-            mask = (v < saturation_param).astype(np.uint8)
+            mask = (v < saturation).astype(np.uint8)
 
     # remove small regions from mask
-    disk_elem = disk(remove_disk_param)
+    disk_elem = disk(remove)
     opened = opening(mask, selem=disk_elem)
 
     # rejoin colored points
-    square_elem = square(rejoin_square_param)
+    square_elem = square(rejoin)
     dilated = dilation(opened, selem=square_elem)
 
     if show_mask:
@@ -110,11 +110,31 @@ def create_mask(img_rgb, type='v', equation='>', saturation=0.75, remove=0, rejo
     return dilated
 
 
-# =========== HOUSES ============
-"""
-TODO:
-- ( fix white houses )
-"""
+def draw_contours(img, contours, color='r', width=3):
+    fig, ax, = plt.subplots()
+    ax.imshow(img, interpolation='nearest', cmap=plt.cm.gray)
+    for n, contour in enumerate(contours):
+        ax.plot(contour[:, 1], contour[:, 0], color, width)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.show()
+
+
+# =========== COUNTERS ============
+
+# FINISHED
+def detect_circle_counters(img):
+    img = exposure.equalize_hist(img)
+    img_black = img
+
+    func = create_mask(img, 's', '>', 0.4, 0, 4).astype(bool)
+    img_black[func, :] = 0
+    contours = measure.find_contours(rgb2gray(img_black), 0.2)
+
+    circle_contours = check_if_circle(contours)
+    circle_contours = find_n_greatest_areas(circle_contours, n_times=15)
+
+    return circle_contours
 
 
 # FINISHED
@@ -124,19 +144,10 @@ def detect_orange_houses(img):
     contours = measure.find_contours(func, 0.05)
     new_contours = find_n_greatest_areas(contours, n_times=3)
 
-    # ======= REMOVE =======
-    fig, ax, = plt.subplots()
-    ax.imshow(img, interpolation='nearest', cmap=plt.cm.gray)
-    for n, contour in enumerate(new_contours):
-        ax.plot(contour[:, 1], contour[:, 0], 'orange', linewidth=3)
-    ax.set_xticks([])
-    ax.set_yticks([])
-    plt.show()
-    # ======= REMOVE =======
     return new_contours
 
 
-# FINISHED
+# TODO FIX ORANGE
 def detect_blue_orange_houses(img):
     func = create_mask(img, 's', '>', 0.8, 2, 10)
 
@@ -163,13 +174,14 @@ def detect_blue_orange_houses(img):
         else:
             # overwrite field's color by drawing r=100px circle
             img[rr2, cc2, :] = (1, 1, 1)
-        # print(detected_extra_orange)
 
     if detected_extra_orange:
         orange_contours = detect_orange_houses(img)
-        return [orange_contours, new_contours]
+        for item in orange_contours:
+            new_contours.append(item)
+        return (new_contours, True)
     else:
-        return [orange_contours]
+        return (new_contours, False)
 
 
 # FINISHED
@@ -183,7 +195,7 @@ def detect_red_houses(img):
     return new_contours
 
 
-# FINISHED BUT WORKING BAD
+# TODO: FIX PARAMS
 def detect_white_houses(img):
     img = exposure.adjust_gamma(img, 2, 1)
     func = create_mask(img, 's', '<', 0.05, 0, 1).astype(bool)
@@ -214,65 +226,264 @@ def detect_black_joker(img):
     return new_contours
 
 
-# =========== FIELDS ============
-"""
-TODO;
-- fix mountain
-- find grass --> extra saturation
-- find grain --> lightness without desert and mountains
-- find clay --> saturation + red color
-"""
+# TODO: CHECK WITH ORANGE DETECTION
+def draw_counters_and_circles(img, img_origin, white=True, red=True, black=True, orange=True, circle=True, fields=True):
+    fig, ax, = plt.subplots(1, 2)
+    img_source = img.copy()
+    # original image
+    ax[0].imshow(img_origin, interpolation='nearest', cmap=plt.cm.gray)
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+
+    # draw white houses
+    if white:
+        for n, contour in enumerate(detect_white_houses(img_source)):
+            centroid = np.mean(contour, axis=0)
+            rr, cc = draw.circle(float(centroid[0]), float(centroid[1]), 90, img_source.shape)
+            img[rr, cc, :] = (255, 255, 255)
+
+    # draw red houses
+    if red:
+        for n, contour in enumerate(detect_red_houses(img_source)):
+            centroid = np.mean(contour, axis=0)
+            rr, cc = draw.circle(float(centroid[0]), float(centroid[1]), 90, img_source.shape)
+            img[rr, cc, :] = (255, 20, 20)
+
+    # draw black joker
+    if black:
+        for n, contour in enumerate(detect_black_joker(img_source)):
+            centroid = np.mean(contour, axis=0)
+            rr, cc = draw.circle(float(centroid[0]), float(centroid[1]), 90, img_source.shape)
+            img[rr, cc, :] = (0, 0, 0)
+            cv2.putText(img, 'PUSTYNIA', (int(centroid[1]), int(centroid[0])), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 6)
+
+    # draw orange and blue houses
+    if orange:
+        blue_and_orange = detect_blue_orange_houses(img_source)
+        if blue_and_orange[1]:
+            for n, contour in enumerate(blue_and_orange[0]):
+                centroid = np.mean(contour, axis=0)
+                rr, cc = draw.circle(float(centroid[0]), float(centroid[1]), 90, img_source.shape)
+                if n > 2:
+                    img[rr, cc, :] = (250, 178, 53)
+                else:
+                    img[rr, cc, :] = (62, 120, 250)
+        else:
+            for n, contour in enumerate(blue_and_orange[0]):
+                centroid = np.mean(contour, axis=0)
+                rr, cc = draw.circle(float(centroid[0]), float(centroid[1]), 90, img_source.shape)
+                img[rr, cc, :] = (250, 178, 53)
+
+    if fields:
+        for n, contour in enumerate(detect_mountain_field(img_source)):
+            centroid = np.mean(contour, axis=0)
+            cv2.putText(
+                img, 'GORY', (int(centroid[1]), int(centroid[0])), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 8)
+        for n, contour in enumerate(detect_clay_field(img_source)):
+            centroid = np.mean(contour, axis=0)
+            cv2.putText(
+                img, 'KOPALNIA', (int(centroid[1]), int(centroid[0])), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 8)
+        for n, contour in enumerate(detect_grass_field(img_source)):
+            centroid = np.mean(contour, axis=0)
+            cv2.putText(
+                img, 'TRAWA', (int(centroid[1]), int(centroid[0])), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 8)
+        for n, contour in enumerate(detect_forest_field(img_source)):
+            centroid = np.mean(contour, axis=0)
+            cv2.putText(
+                img, 'LAS', (int(centroid[1]), int(centroid[0])), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 8)
 
 
-# FINISHED
-def detect_circle_counters(img):
-    img = exposure.equalize_hist(img)
-    img_black = img
+    # show image on ax
+    ax[1].imshow(img, interpolation='nearest', cmap=plt.cm.gray)
 
-    func = create_mask(img, 's', '>', 0.4, 0, 4).astype(bool)
-    img_black[func, :] = 0
-    contours = measure.find_contours(rgb2gray(img_black), 0.2)
+    # draw circle counters
+    if circle:
+        for n, contour in enumerate(detect_circle_counters(img_source)):
+            ax[1].plot(contour[:, 1], contour[:, 0], 'navy', linewidth=3)
+        for n, contour in enumerate(create_mountain_circles(img_source)):
+            ax[1].plot(contour[:, 1], contour[:, 0], 'navy', linewidth=3)
 
-    circle_contours = check_if_circle(contours)
-    circle_contours = find_n_greatest_areas(circle_contours, n_times=15)
-
-    return circle_contours
-
-# TODO
-def detect_mountain_field(img):
-    # when image around circle is '0' -> this is mountain filed
-    func = create_mask(img, 's', '<', 0.25, 3, 1).astype(bool)
-    img[func, :] = 0
-
-    io.imshow(img)
+    # show plot
+    ax[1].set_xticks([])
+    ax[1].set_yticks([])
     plt.show()
 
 
+# =========== FIELDS ============
+
+
 # FINISHED
-def detect_forest_field(img):
+def detect_mountain_field(img):
+    # when image around circle is '0' -> this is mountain filed
+    img_source = img
+    img = exposure.equalize_hist(img)
+    func = create_mask(img, 'h', '<', 0.55, 5, 5).astype(bool)
+    img[func, :] = 255
+    func = create_mask(img, 'h', '>', 0.8, 0, 5).astype(bool)
+    img[func, :] = 255
+    img = 255 - img
+
+    contours = measure.find_contours(rgb2gray(img), 0.2)
+
+    new_contours = check_if_circle(contours, 100, 1000, 4000)
+    contours = find_n_greatest_areas(new_contours, n_times=3)
+
+    # draw_contours(img_source, contours)
+
+    return contours
+
+
+# FINISHED
+def create_mountain_circles(img):
+    contours = detect_mountain_field(img)
+    img2 = img - img
+    for contour in contours:
+        centroid = np.mean(contour, axis=0)
+        rr, cc, = draw.circle(int(centroid[0]), int(centroid[1]), 70, img.shape)
+        img2[rr, cc] = (255, 255, 255)
+
+    new_contours = measure.find_contours(rgb2gray(img2), 0.2)
+    # draw_contours(img, new_contours, 'navy')
+
+    return new_contours
+
+
+# FINISHED
+def detect_forest_field(img2):#, circles):
     # when mean of image around circle is > '0' / < '255' -> this is forest filed
     # could also return (255-img) - may be easier to detect
+    img = img2.copy()
     func = create_mask(img, 'v', '>', 0.55, 0, 2).astype(bool)
     img[func, :] = 0
     # additional color mask
     func = create_mask(img, 'h', '<', 0.1, 0, 1).astype(bool)
     img[func, :] = 0
 
+    greenmask = img < 100
+    img[greenmask] = 0
+
+    greenmask = img > 100
+    img[greenmask] = 255
+
+    img = dilation(img)
+
+    contours = measure.find_contours(rgb2gray(img), 0.1)
+
+    new_contours = check_if_circle(contours, 50, 300, 4000)
+    new_contours = find_n_greatest_areas(new_contours, n_times=4)
+    #draw_contours(img_source, contours)
+
+    return new_contours
+
+    # forest_fields = []
+    # for circle in circles:
+    #     centroid = np.mean(circle, axis=0)
+    #     rr, cc = draw.circle_perimeter(int(centroid[0]), int(centroid[1]), 200)
+    #     # img[rr, cc, :] = (0, 0, 0)
+    #     imghsv = rgb2hsv(img)
+    #     h = imghsv[rr, cc, 0]
+    #     h = np.interp(h, [0, 1], [0, 255])
+    #     if h.mean() > 10:
+    #         forest_fields.append(circle)
+    # return forest_fields
+
+
+
+# TODO GRAIN
+def detect_grain_field(img):
+    img_source = img
+    img = exposure.equalize_hist(img)
+    func = create_mask(img, 'h', '>', 0.15, 1, 1).astype(bool)
+    img[func, :] = 0
     io.imshow(img)
     plt.show()
 
+    # func = create_mask(img, 'v', '<', 0.8, 1, 5).astype(bool)
+    io.imshow(img)
+    plt.show()
+
+    # grain_field = []
+    # for circle in circles:
+    #     centroid = np.mean(circle, axis=0)
+    #     rr, cc = draw.circle_perimeter(int(centroid[0]), int(centroid[1]), 150, shape=img.shape)
+    #
+    #     imghsv = rgb2hsv(img)
+    #     h = imghsv[rr, cc, 0]
+    #     v = imghsv[rr, cc, 2]
+    #     h = np.interp(h, [0, 1], [0, 255])
+    #     v = np.interp(v, [0, 1], [0, 255])
+    #
+    #     mean_h = h.mean()
+    #     if mean_h > 0:
+    #         grain_field.append([v.mean(), circle])
+    #
+    # longest = []
+    # if len(grain_field) > 4:
+    #     newlist = sorted(grain_field, key=itemgetter(0))
+    #     for i in range(4):
+    #         longest.append(newlist[-(i + 1)][1])
+    # else:
+    #     for item in grain_field:
+    #         longest.append(item[1])
+    #
+    # draw_contours(img_source, longest)
+    # return longest
 
 
+# FINISHED
+def detect_clay_field(img):
+    img_source = img
+    img = exposure.equalize_hist(img)
+    func = create_mask(img, 'h', '>', 0.08, 5, 5).astype(bool)
+    img[func, :] = 255
+    img = 255 - img
 
+    contours = measure.find_contours(rgb2gray(img), 0.2)
+    new_contours = check_if_circle(contours, 100, 1500, 4000)
+    new_contours = find_n_greatest_areas(new_contours, n_times=3)
+    # draw_contours(img_source, new_contours, 'navy')
+    # io.imshow(img)
+    # plt.show()
+    return new_contours
+
+
+# FINISHED
+def detect_grass_field(img):
+    # when image around circle is '0' -> this is mountain filed
+    img_source = img
+    img = exposure.equalize_hist(img)
+    img = exposure.adjust_gamma(img, 2, 1)
+    func = create_mask(img, 'h', '<', 0.25, 1, 1).astype(bool)
+    img[func, :] = 0
+    func = create_mask(img, 'h', '>', 0.35, 0, 1).astype(bool)
+    img[func, :] = 0
+    func = create_mask(img, 'v', '<', 0.4, 0, 5).astype(bool)
+    img[func, :] = 0
+    # io.imshow(img)
+    #     # plt.show()
+
+    contours = measure.find_contours(rgb2gray(img), 0.2)
+    new_contours = check_if_circle(contours, 100, 200, 5000)
+    new_contours = find_n_greatest_areas(new_contours, n_times=4)
+    # draw_contours(img_source, new_contours, 'navy')
+    return new_contours
 
 
 # -------START
+
 names = ['ex1', 'ex2', 'ex3', 'ex4', 'ex5', 'ex6', 'ex71', 'ex72']
-for name in names:
-    img_name = name+'.jpg'
+medium = ['ex1', 'ex3', 'ex4', 'ex72']
+hard = ['ex3', 'ex4', 'ex72']
+blue = ['ex3', 'ex71', 'ex72']
+newlist = ['ex81']
+
+for name in newlist:
+    img_name = name + '.jpg'
     img = io.imread(os.path.join('resources/', img_name))
     img_board = BoardAssignment(img)
-    detect_circle_counters(img_board)
+    draw_counters_and_circles(img_board, img, red=True, white=False, black=True, circle=True, orange=True, fields=True)
+    #detect_grain_field(img_board)
+
 # -------END
 
 
